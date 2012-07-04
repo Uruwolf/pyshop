c76d024f6344ba4a6759a2fb7edd8fa99c87cc31
Steve Uru West <uruwolf@gmail.com>
2012-07-04 17:17:19 +0100

    Added a global package for common functions and updated the product detail template to optionally include the 'add to cart' snippit

---
+++ global/templatetags/global_tags.py
@@ -0,0 +1,31 @@
+# Stolen from: http://bruno.im/2009/dec/07/silently-failing-include-tag-in-django/
+# Big thanks for this!
+
+from django import template
+
+register = template.Library()
+
+class IncludeNode(template.Node):
+	def __init__(self, template_name):
+		self.template_name = template_name
+	
+	def render(self, context):
+		try:
+			# Loading template and rendering it
+			included_template = template.loader.get_template(self.template_name).render(context)
+		except template.TemplateDoesNotExist:
+			included_template = ''
+		return included_template
+
+@register.tag
+def try_to_include(parser, token):
+	"""Usage: {% try_to_include "head.html" %}
+
+	This will fail silently if the template doesn't exist. If it does, it will
+	be rendered with the current context."""
+	try:
+		tag_name, template_name = token.split_contents()
+	except ValueError:
+		"%r tag requires a single argument" % token.contents.split()[0]
+
+	return IncludeNode(template_name[1:-1])
